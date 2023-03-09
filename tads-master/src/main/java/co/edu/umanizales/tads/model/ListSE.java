d0312926db984ea8e00bcd065fae5576b5d66f1d
jvalencia99 <127439985+jvalencia99@users.noreply.github.com>
2023-03-09 14:50:55 -0500

    Add files via upload

---
+++ tads-master/src/main/java/co/edu/umanizales/tads/model/ListSE.java
@@ -0,0 +1,61 @@
+package co.edu.umanizales.tads.model;
+
+import lombok.Data;
+
+@Data
+public class ListSE {
+    private Node head;
+    /*
+    Algoritmo de adicionar al final
+    Entrada
+        un niño
+    si hay datos
+    si
+        llamo a un ayudante y le digo que se posicione en la cabeza
+        mientras en el brazo exista algo
+            pasese al siguiente
+        va estar ubicado en el ùltimo
+
+        meto al niño en un costal (nuevo costal)
+        y le digo al ultimo que tome el nuevo costal
+    no
+        metemos el niño en el costal y ese costal es la cabeza
+     */
+    public void add(Kid kid){
+        if(head != null){
+            Node temp = head;
+            while(temp.getNext() !=null)
+            {
+                temp = temp.getNext();
+            }
+            /// Parado en el último
+            Node newNode = new Node(kid);
+            temp.setNext(newNode);
+        }
+        else {
+            head = new Node(kid);
+        }
+    }
+
+    /* Adicionar al inicio
+    si hay datos
+    si
+        meto al niño en un costal (nuevocostal)
+        le digo a nuevo costal que tome con su brazo a la cabeza
+        cabeza es igual a nuevo costal
+    no
+        meto el niño en un costal y lo asigno a la cabez
+     */
+    public void addToStart(Kid kid){
+        if(head !=null)
+        {
+            Node newNode = new Node(kid);
+            newNode.setNext(head);
+            head = newNode;
+        }
+        else {
+            head = new Node(kid);
+        }
+    }
+    
+}
